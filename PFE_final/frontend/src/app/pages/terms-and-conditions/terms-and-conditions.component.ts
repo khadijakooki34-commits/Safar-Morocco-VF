3b0d6bc7c265fd85494b72041fc83c3478539ec8
khadija El Achhab <khadijakooki34@gmail.com>
2026-03-26 00:42:06 +0100

    update terms commit

--- PFE_final/frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ PFE_final/frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-terms-and-conditions',
   standalone: false,
@@ -8,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class TermsAndConditionsComponent {
   currentDate = new Date();
+
+  constructor(public translate: TranslateService) {}
+
+  get currentLang(): string {
+    return this.translate.currentLang || this.translate.getDefaultLang() || 'en';
+  }
 }
